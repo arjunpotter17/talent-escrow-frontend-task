41e55a7b339f1290a19ca1137dab270c84223025
arjunpotter17 <arjunpotter17@gmail.com>
2024-07-23 16:32:19 +0530

    bg fix

--- src/app/components/CreateEscrowForm.tsx
+++ src/app/components/CreateEscrowForm.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { TokenDropdown, AmountInput, MintAddressForm } from './index';
-import { TokenData } from '../types';
-import Spinner from './Spinner/Spinner';
+import React from "react";
+import { TokenDropdown, AmountInput, MintAddressForm } from "./index";
+import { TokenData } from "../types";
+import Spinner from "./Spinner/Spinner";
 
 interface CreateEscrowFormProps {
   tokens: TokenData[];
@@ -17,7 +17,6 @@ interface CreateEscrowFormProps {
   setAmountToSend: (value: number) => void;
   setReceiveMintAddress: (value: string) => void;
   setReceiveAmount: (value: number) => void;
-
 }
 
 const CreateEscrowForm: React.FC<CreateEscrowFormProps> = ({
@@ -33,16 +32,36 @@ const CreateEscrowForm: React.FC<CreateEscrowFormProps> = ({
   setReceiveMintAddress,
   setReceiveAmount,
   loading,
-  setLoading
+  setLoading,
 }) => {
   return (
     <form className="w-full max-w-lg">
       <TokenDropdown tokens={tokens} handleTokenChange={handleTokenChange} />
-      <AmountInput id="amount-to-send" label="Amount to Send" value={amountToSend} onChange={(e) => setAmountToSend(Number(e.target.value))} />
-      <MintAddressForm id="receive-mint-address" label="Receive Mint Address" value={receiveMintAddress} onChange={(e) => setReceiveMintAddress(e.target.value)} />
-      <AmountInput id="receive-amount" label="Receive Amount" value={receiveAmount} onChange={(e) => setReceiveAmount(Number(e.target.value))} />
-      <button type="button" disabled={disabled} onClick={handleCreateEscrow} className="bg-toekn-orange hover:bg-toekn-dark-orange text-white font-toekn-regular py-2 px-4 w-[150px]">
-        {loading ? <Spinner size={25} color='#fff'/> : "Create Escrow"}
+      <AmountInput
+        id="amount-to-send"
+        label="Amount to Send"
+        value={amountToSend}
+        onChange={(e) => setAmountToSend(Number(e.target.value))}
+      />
+      <MintAddressForm
+        id="receive-mint-address"
+        label="Receive Mint Address"
+        value={receiveMintAddress}
+        onChange={(e) => setReceiveMintAddress(e.target.value)}
+      />
+      <AmountInput
+        id="receive-amount"
+        label="Receive Amount"
+        value={receiveAmount}
+        onChange={(e) => setReceiveAmount(Number(e.target.value))}
+      />
+      <button
+        type="button"
+        disabled={disabled}
+        onClick={handleCreateEscrow}
+        className="bg-toekn-orange hover:bg-toekn-dark-orange text-white font-toekn-regular py-2 px-4 w-[150px]"
+      >
+        {loading ? <Spinner size={25} color="#fff" /> : "Create Escrow"}
       </button>
     </form>
   );
